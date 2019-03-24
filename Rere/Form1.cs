3f6c8d1d1405718ab97f4fab923efe7a1cb8e183
Welldone <Welldone@DESKTOP-OPDMU0U>
2019-03-24 07:02:19 +0500

    comet

--- Rere/Form1.cs
+++ Rere/Form1.cs
@@ -11,23 +11,88 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Rere
 {
     public partial class Form1 : Form
     {
+        bool[,] r = new bool[10, 10];
+        bool[,] l = new bool[10, 10];
+        
+
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i == j || i == j + 1 || j == i + 1
+                        || i == j + 2 || j == i + 2
+                        || i == j - 1 || j == i - 1
+                        || i == j - 2 || j == i - 2)
+                    {
+                        r[i, j] = true;
+                    }
+                }
+            }
+            l = r;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if( j < 6)
+                    {
+                        bool temp = false;
+                        temp = l[i, j];
+                        l[i, j] = l[i, 9 - j];
+                        l[i, 9 - j] = temp;
+                    }
+                }
+            }
         }
         Image<Bgr, byte> imgImage;
-
         
+        bool CheckMatr(Bitmap picture, int x, int y)
+        {
+            int w = picture.Width;
+            int h = picture.Height;
+
+            bool[,] temp = new bool[h, w];
+            Color clr = new Color();
+
+            for(int i = y; i < x + h; i++)
+            {
+                for(int j = x; j < x + w; j++)
+                {
+                    clr = picture.GetPixel(i, j);
+                    if (clr.Name == "ffffffff") temp[i, j] = true;
+                    else temp[i, j] = false;
+                }
+            }
+
+            int cnt = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    if (temp[i, j] == r[i, j])
+                        cnt++;
+                }
+            }
+
+            if (cnt >= 10) return true;
+
+            return false;
+        }
+
         private void button1_Click_1(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                imgImage = new Image<Bgr, byte>(ofd.FileName); 
+                imgImage = new Image<Bgr, byte>(ofd.FileName);
                 pictureBox1.Image = imgImage.Bitmap;
             }
 
@@ -41,87 +106,47 @@ namespace Rere
             pictureBox1.Height = imgout.Height;
             pictureBox1.Image = imgout.Bitmap;
 
-            int w_l = 60; // на какое кол-во кусков будет делиться картинка
-            int h_l = 50; 
+            int w_l = 10; // на какое кол-во кусков будет делиться картинка
+            int h_l = 10;
 
             int W = pictureBox1.Width / w_l; // ширина куска
             int H = pictureBox1.Height / h_l; // длина куска
-
-            List<Point> countours = new List<Point>();
-           
+            
             List<Image> imageParts = new List<Image>();
-
-            List<Arr> ZeroOne = new List<Arr>();
-
-            int n = 4;
+            
+            int n = 2300;
             for (int i = 0; i < h_l; i++)
             {
-                for(int j = 0; j < w_l; j++)
+                for (int j = 0; j < w_l; j++)
                 {
-                    Rectangle rectangle = new Rectangle(W * j, H * i, W, H);
+                    Rectangle rectangle = new Rectangle(W * j, H * i, w_l, h_l);
                     var pic = (Bitmap)pictureBox1.Image;
                     imageParts.Add(pic.Clone(rectangle, PixelFormat.Format16bppRgb555));
-                    Point temp = new Point(i * H, j * W);
-                    ZeroOne.Add(ConvertToArray(pic.Clone(rectangle, PixelFormat.Format16bppRgb555), temp, W, H));
                 }
             }
 
-            
+            bool[,] ar = new bool[10, 10];
 
-            for (int i = 0; i < ZeroOne[n].h; i++)
+            int cnt = 0;
+
+            List<Image> ans = new List<Image>();
+
+            for(int i = 0; i < pictureBox1.Height - 10; i++)
             {
-                for (int j = 0; j < ZeroOne[n].w; j++)
+                for(int j = 0; j < pictureBox1.Width - 10; j++)
                 {
-                    if (ZeroOne[n].array[i, j])
-                        richTextBox1.Text += 1;
-                    else
-                        richTextBox1.Text += 0;
-                }
-                richTextBox1.Text += Environment.NewLine;
-            }
-
-            pictureBox2.Image = imageParts[n];
-
-            for (int i = ZeroOne[n].y; i < H; i++)
-            {
-                for (int j = ZeroOne[n].x; j < W; j++)
-                {
-                    if(ZeroOne[n].array[i,j])
+                    if (CheckMatr(new Bitmap(imageParts[cnt]), i, j))
                     {
-                        Console.WriteLine("1");
+                        ans.Add(imageParts[cnt]);
                     }
-                    else
-                        Console.WriteLine("0");
+                    cnt++;
                 }
-                Console.WriteLine("\n");
-            }
-        }
-
-        public Arr ConvertToArray(Image img, Point p, int W, int H)
-        {
-            Bitmap picture = new Bitmap(img);
-            Arr a = new Arr(W, H, p.X, p.Y);
-            Color clr = new Color();
-            for(int i = p.Y; i < p.Y + H; i++)
-            {
-                for(int j = p.X; j < p.X + W; j++ )
-                {
-                    clr = picture.GetPixel(i, j);
-                    if(clr.Name == "ffffffff")
-                    {
-                        a.array[i, j] = true;            
-                    }
-                    else
-                    {
-                        a.array[i, j] = false;
-                    }
-                }
-            }
-            
-            return a;
+            }         
         }
     }
 }
+
+
 
 public struct Arr
 {
@@ -136,4 +161,41 @@ public struct Arr
     public bool[,] array;
     public int x, y;
     public int w, h;
+}
+
+class LagrangeInterpolator
+{
+    private readonly double[] xValues;
+    private readonly double[] coefficients;
+
+    public LagrangeInterpolator(double[] xValues, double[] yValues)
+    {
+        if (xValues.Length != yValues.Length) throw new ArgumentException();
+        this.xValues = (double[])xValues.Clone();
+        coefficients = new double[xValues.Length];
+        for (int i = 0; i < xValues.Length; i++)
+        {
+            coefficients[i] = yValues[i];
+            for (int j = 0; j < xValues.Length; j++)
+            {
+                if (i != j) coefficients[i] /= (xValues[i] - xValues[j]);
+            }
+        }
+    }
+
+    public double f(double x)
+    {
+        double result = 0.0;
+        for (int i = 0; i < xValues.Length; i++)
+        {
+            double s = coefficients[i];
+            for (int j = 0; j < xValues.Length; j++)
+            {
+                if (i != j) s *= (x - xValues[j]);
+            }
+            result += s;
+        }
+        return result;
+    }
+
 }
