70d5100fb47ae2338571f74179806e460e15f9b7
Welldone <Welldone@DESKTOP-OPDMU0U>
2019-03-24 09:01:16 +0500

    AlmostDone

--- Rere/Form1.cs
+++ Rere/Form1.cs
@@ -10,22 +10,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 
 namespace Rere
 {
     public partial class Form1 : Form
     {
-        bool[,] r = new bool[10, 10];
-        bool[,] l = new bool[10, 10];
+        const int k = 30;
+        bool[,] r = new bool[k, k];
+        bool[,] l = new bool[k, k];
         
 
         public Form1()
         {
             InitializeComponent();
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < k; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < k; j++)
                 {
                     if (i == j || i == j + 1 || j == i + 1
                         || i == j + 2 || j == i + 2
@@ -37,22 +39,22 @@ namespace Rere
                 }
             }
             l = r;
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < k; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < k; j++)
                 {
                     if( j < 6)
                     {
                         bool temp = false;
                         temp = l[i, j];
-                        l[i, j] = l[i, 9 - j];
-                        l[i, 9 - j] = temp;
+                        l[i, j] = l[i, k - 1 - j];
+                        l[i, k - 1 - j] = temp;
                     }
                 }
             }
         }
         Image<Bgr, byte> imgImage;
-        
+
         bool CheckMatr(Bitmap picture, int x, int y)
         {
             int w = picture.Width;
@@ -61,28 +63,61 @@ namespace Rere
             bool[,] temp = new bool[h, w];
             Color clr = new Color();
 
-            for(int i = y; i < x + h; i++)
+            for (int i = 0; i < h; i++)
             {
-                for(int j = x; j < x + w; j++)
+                for (int j = 0; j < w; j++)
                 {
                     clr = picture.GetPixel(i, j);
-                    if (clr.Name == "ffffffff") temp[i, j] = true;
+                    if (clr.Name == "ffffffff")
+                    {
+                        temp[i, j] = true;
+                    }
                     else temp[i, j] = false;
                 }
             }
 
             int cnt = 0;
 
-            for(int i = 0; i < 10; i++)
+            for (int i = 0; i < k; i++)
             {
-                for(int j = 0; j < 10; j++)
+                for (int j = 0; j < k; j++)
                 {
                     if (temp[i, j] == r[i, j])
                         cnt++;
                 }
             }
 
-            if (cnt >= 10) return true;
+            if (cnt >= k)
+            {
+                y += h;
+                for(int k = 0; k < pictureBox1.Height - h; k++)
+                {
+                    cnt = 0;
+                    for (int i = 0; i <  h; i++)
+                    {
+                        for (int j = 0; j < w; j++)
+                        {
+                            clr = picture.GetPixel(i, j);
+                            if (clr.Name == "ffffffff")
+                            {
+                                temp[i, j] = true;
+                            }
+                            else temp[i, j] = false;
+                        }
+                    }
+                    y += k;
+
+                    for (int i = 0; i < k; i++)
+                    {
+                        for (int j = 0; j < k; j++)
+                        {
+                            if (temp[i, j] == r[i, j])
+                                cnt++;
+                        }
+                    }
+                    if (cnt >= 2 * k) return true;     
+                }
+            }
 
             return false;
         }
@@ -106,43 +141,38 @@ namespace Rere
             pictureBox1.Height = imgout.Height;
             pictureBox1.Image = imgout.Bitmap;
 
-            int w_l = 10; // на какое кол-во кусков будет делиться картинка
-            int h_l = 10;
+            int w_l = k; // на какое кол-во кусков будет делиться картинка
+            int h_l = k;
 
             int W = pictureBox1.Width / w_l; // ширина куска
             int H = pictureBox1.Height / h_l; // длина куска
             
             List<Image> imageParts = new List<Image>();
             
-            int n = 2300;
-            for (int i = 0; i < h_l; i++)
-            {
-                for (int j = 0; j < w_l; j++)
-                {
-                    Rectangle rectangle = new Rectangle(W * j, H * i, w_l, h_l);
-                    var pic = (Bitmap)pictureBox1.Image;
-                    imageParts.Add(pic.Clone(rectangle, PixelFormat.Format16bppRgb555));
-                }
-            }
-
-            bool[,] ar = new bool[10, 10];
-
-            int cnt = 0;
-
             List<Image> ans = new List<Image>();
-
-            for(int i = 0; i < pictureBox1.Height - 10; i++)
+            for (int i = 0; i < pictureBox1.Height - h_l; i += 20)
             {
-                for(int j = 0; j < pictureBox1.Width - 10; j++)
+                for (int j = 0; j < pictureBox1.Width - w_l; j += 20)
                 {
-                    if (CheckMatr(new Bitmap(imageParts[cnt]), i, j))
+                    Rectangle rectangle = new Rectangle(j, i, w_l, h_l);
+                    var pic = (Bitmap)pictureBox1.Image;
+                    Image img = pic.Clone(rectangle, pic.PixelFormat);
+                    if (CheckMatr(new Bitmap(img), i, j))
                     {
-                        ans.Add(imageParts[cnt]);
+                        pictureBox2.Image = img;
+                        Refresh();
+                        Graphics g = this.CreateGraphics();
+
+                        g.FillEllipse(new SolidBrush(Color.Red), i, j, 10, 10);
+                        ans.Add(img);
+                        richTextBox1.Text += "x: " + i + " y: " + j;
+                        richTextBox1.Text += Environment.NewLine;
                     }
-                    cnt++;
                 }
-            }         
+                }
+            
         }
+        
     }
 }
 
